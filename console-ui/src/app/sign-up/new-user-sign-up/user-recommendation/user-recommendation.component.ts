5762ed565eb17061cacbcd72df6b5eb88f837561
Akshay <akshay.kumar@ireslab.com>
2018-04-02 10:25:41 +0530

    new user recommendation UI (create some common div)

--- console-ui/src/app/sign-up/new-user-sign-up/user-recommendation/user-recommendation.component.ts
+++ console-ui/src/app/sign-up/new-user-sign-up/user-recommendation/user-recommendation.component.ts
@@ -8,17 +8,55 @@ declare var $:any
   styleUrls: ['./user-recommendation.component.css'],
 })
 export class UserRecommendationComponent implements OnInit {
+
+    dwellingTypes = ['HDB 1/2','HDB 3','HDB 4','HDB 5','Condominium','Landed Property','Others']
+    dwlTypePlans  = [  
+                        {"planName":"SMART", "monthlyBill":"$59*","discount":"17% LESS","details":['24 Months','Peak Rate (7am to 11pm): $0.1/kwh','Off Peak Rate(11pm to 7am): $0.01/kwh','All charges are absorbed','Consumption is loss adjusted']},  
+                        {"planName":"CLEAN", "monthlyBill":"$59*","discount":"19% LESS","details":['24 Months','Peak Rate (7am to 11pm): $0.1/kwh','Off Peak Rate(11pm to 7am): $0.01/kwh','All charges are absorbed','Consumption is loss adjusted']},  
+                        {"planName":"SMART", "monthlyBill":"$59*","discount":"17% LESS","details":['24 Months','Peak Rate (7am to 11pm): $0.1/kwh','Off Peak Rate(11pm to 7am): $0.01/kwh','All charges are absorbed','Consumption is loss adjusted']},  
+                        {"planName":"CLEAN", "monthlyBill":"$59*","discount":"19% LESS","details":['24 Months','Peak Rate (7am to 11pm): $0.1/kwh','Off Peak Rate(11pm to 7am): $0.01/kwh','All charges are absorbed','Consumption is loss adjusted']},  
+                        {"planName":"SMART", "monthlyBill":"$59*","discount":"17% LESS","details":['24 Months','Peak Rate (7am to 11pm): $0.1/kwh','Off Peak Rate(11pm to 7am): $0.01/kwh','All charges are absorbed','Consumption is loss adjusted']},  
+                        {"planName":"CLEAN", "monthlyBill":"$59*","discount":"19% LESS","details":['24 Months','Peak Rate (7am to 11pm): $0.1/kwh','Off Peak Rate(11pm to 7am): $0.01/kwh','All charges are absorbed','Consumption is loss adjusted']},  
+                        {"planName":"SMART", "monthlyBill":"$59*","discount":"17% LESS","details":['24 Months','Peak Rate (7am to 11pm): $0.1/kwh','Off Peak Rate(11pm to 7am): $0.01/kwh','All charges are absorbed','Consumption is loss adjusted']},  
+                        {"planName":"CLEAN", "monthlyBill":"$59*","discount":"19% LESS","details":['24 Months','Peak Rate (7am to 11pm): $0.1/kwh','Off Peak Rate(11pm to 7am): $0.01/kwh','All charges are absorbed','Consumption is loss adjusted']}  
+                  ] 
+    firstTwoDwlingPlans:any = [];
+    otherdwlPlans:any = [];
+
     constructor(private sbService:SidebarService) {
         this.sbService.getSidebar("newUser")
     }
 
+  
+  firstTwoDwlPlans(){
+      for (let index = 0; index < this.dwlTypePlans.length; index++) {
+          if (index < 2){
+            this.firstTwoDwlingPlans[index] = this.dwlTypePlans[index];
+          }else{
+              this.otherdwlPlans[index-2] = this.dwlTypePlans[index];
+          }
+      }
+  }
+ 
+  planSelected(){
+       this.firstTwoDwlPlans();
+  }
+               
+
+
+
+
+
+
+
   ngOnInit() {
+    
     $(document).ready(function () {
       //Script on Page Load
-      $("button.selectPlanButton").on("click", function () {
+      $('button.selectPlanButton').on("click", function () {
           //Reset the selections
-          $("button.selectPlanButton").removeClass("btn-primary");
-          $("button.selectPlanButton").addClass("btn-secondary");
+          $('button.selectPlanButton').removeClass("btn-primary");
+          $('button.selectPlanButton').addClass("btn-secondary");
           $(".PlanDetailCard").removeClass("selected");
 
           //Highlight the correct panel
@@ -38,6 +76,7 @@ export class UserRecommendationComponent implements OnInit {
           $('button.selectPropertyTypeButton').removeClass("selected");
           $(this).addClass("selected");
           $('#Grp-Package').show();
+          
       });
       $("#Grp-DwellingType-MobileSelect").change(function () {
           $('#Grp-Package').show();
