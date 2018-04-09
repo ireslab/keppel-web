9e89673af945728add75b7d79dc833d12de90353
Hitesh <hitesh.harit@ireslab.com>
2018-04-09 10:12:55 +0530

    *contract form integration *shared service integration

--- console-ui/src/app/sign-up/new-user-sign-up/user-confirmation/user-confirmation.component.ts
+++ console-ui/src/app/sign-up/new-user-sign-up/user-confirmation/user-confirmation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SidebarService } from '../../../sidebar/sidebar.service';
+import { DataShare } from '../../../Utility/data_share.service';
 
 @Component({
   selector: 'app-user-confirmation',
@@ -7,12 +8,18 @@ import { SidebarService } from '../../../sidebar/sidebar.service';
   styleUrls: ['./user-confirmation.component.css']
 })
 export class UserConfirmationComponent implements OnInit {
-
-  constructor(private sbService:SidebarService) {
+  _usderDetailObj = {}
+  constructor(private sbService: SidebarService, private DS: DataShare) {
     this.sbService.getSidebar("newUser")
   }
 
   ngOnInit() {
+    this.DS.userDetails.subscribe(
+      (value) => {
+        this._usderDetailObj = value
+        console.log(value)
+      }
+    )
   }
 
 }
