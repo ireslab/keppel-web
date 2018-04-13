a9bca0fd7ed32f7828147246b26e304d0812f947
Hitesh <hitesh.harit@ireslab.com>
2018-04-13 12:58:01 +0530

    NRIC update validation Address update

--- console-ui/src/app/Utility/utility.class.ts
+++ console-ui/src/app/Utility/utility.class.ts
@@ -1,51 +1,43 @@
 
 export class utilityClass {
 
-// function to validate NRIC number
 
- static isValidNric(theNric: any): boolean {
-    var nric: any = [];
-    nric.multiples = [2, 7, 6, 5, 4, 3, 2];
-
-    if (!theNric || theNric == '') {
+static isValidNric(str:any):boolean {
+    if (str.length != 9) 
         return false;
+
+    str = str.toUpperCase();
+
+    var i, 
+        icArray = [];
+    for(i = 0; i < 9; i++) {
+        icArray[i] = str.charAt(i);
     }
 
-    if (theNric.length != 9) {
-        return false;
+    icArray[1] = parseInt(icArray[1], 10) * 2;
+    icArray[2] = parseInt(icArray[2], 10) * 7;
+    icArray[3] = parseInt(icArray[3], 10) * 6;
+    icArray[4] = parseInt(icArray[4], 10) * 5;
+    icArray[5] = parseInt(icArray[5], 10) * 4;
+    icArray[6] = parseInt(icArray[6], 10) * 3;
+    icArray[7] = parseInt(icArray[7], 10) * 2;
+
+    var weight = 0;
+    for(i = 1; i < 8; i++) {
+        weight += icArray[i];
     }
 
-    var total = 0
-        , count = 0
-        , numericNric;
-    var first = theNric[0]
-        , last = theNric[theNric.length - 1];
+    var offset = (icArray[0] == "T" || icArray[0] == "G") ? 4:0;
+    var temp = (offset + weight) % 11;
 
-    if (first != 'S' && first != 'T') {
-        return false;
-    }
+    var st = ["J","Z","I","H","G","F","E","D","C","B","A"];
+    var fg = ["X","W","U","T","R","Q","P","N","M","L","K"];
 
-    numericNric = theNric.substr(1, theNric.length - 2);
+    var theAlpha;
+    if (icArray[0] == "S" || icArray[0] == "T") { theAlpha = st[temp]; }
+    else if (icArray[0] == "F" || icArray[0] == "G") { theAlpha = fg[temp]; }
 
-    if (isNaN(numericNric)) {
-        return false
-    }
-
-    while (numericNric != 0) {
-        total += (numericNric % 10) * nric.multiples[nric.multiples.length - (1 + count++)];
-
-        numericNric /= 10;
-        numericNric = Math.floor(numericNric);
-    }
-
-    var outputs;
-    if (first == 'S') {
-        outputs = ['J', 'Z', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'];
-    }
-    else {
-        outputs = ['G', 'F', 'E', 'D', 'C', 'B', 'A', 'J', 'Z', 'I', 'H'];
-    }
-    return last == outputs[total % 11];
+    return (icArray[8] === theAlpha);
 }
    
 
